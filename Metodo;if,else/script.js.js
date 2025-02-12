d11bff7f07f81ff83caeb61268df25b80a8487bb
Michele1421 <michelevicino54@gmail.com>
2025-02-12 20:56:20 +0100

    Add files via upload

---
+++ Metodo;if,else/script.js.js
@@ -0,0 +1,19 @@
+
+       
+ let voto = 21; 
+
+if (voto < 18) {
+     console.log("voto:insufficiente");
+ } else if (voto >= 18 && voto < 21) {
+     console.log("voto:sufficiente");
+ } else if (voto >= 21 && voto < 24) {
+     console.log("voto:buono");
+ } else if (voto >= 24 && voto < 27) {
+     console.log("voto:distinto");
+ } else if (voto >= 27 && voto < 30) {
+     console.log("voto:ottimo");
+ } else if (voto = 30) {
+     console.log("voto:eccellente");
+ }
+  
+
---
+++ metodoswitch/script.js.js
@@ -0,0 +1,24 @@
+
+let voto= 18; 
+
+switch (true) {
+    case (voto< 18):
+        console.log("voto:insufficiente");
+        break;
+    case (voto >= 18 && voto < 21):
+        console.log("voto:sufficiente");
+        break;
+    case (voto >= 21 && voto < 24):
+        console.log("voto:buono");
+        break;
+    case (voto >= 24 && voto < 27):
+        console.log("voto:distinto");
+        break;
+    case (voto >= 27 && voto < 30):
+        console.log("voto:ottimo");
+        break;
+    case (voto = 30):
+        console.log("voto:eccellente");
+        break;
+}    
+
